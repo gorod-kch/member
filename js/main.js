59393c687e12f4887fcf01436880725ebb23bbc1
Andrey Polyskalov <contact@polyskalov.com>
2016-12-05 22:05:58 +0500

    Приложение теперь загружается на секунду быстрее

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ $('.splashscreen__close').on('click', function(event) {
         setTimeout(function(){
             $('.preloader').hide();
             $('.app').show();
-        }, 2000);
+        }, 1000);
     }
 
 });
